e12c67815dc41bb4d7f137a42f3205d431e8ccf2
Mame <mamely1847@gmail.com>
2022-01-05 10:38:04 +0100

    setup add

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,19 +3,31 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class tp1 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver(); // instantation pour ouvrir google
+        driver.get("https://www.amazon.fr"); // ouvrir la page google.com
+        driver.manage().window().maximize();// maximiser la taille de l'écran
+        //Fermer le cookies
+        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
+        buttonCookies.click();
+    }
+
     @Test
     public void test1(){
-        WebDriver driver = new ChromeDriver(); // instantation pour ouvrir google
-        driver.get("https://www.amazon.fr"); // ouvrir la page google.com
+
         try {
             Thread.sleep(1000); // Pour instancier un temps d'attente
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.manage().window().maximize();// maximiser la taille de l'écran
+
 
         // Sur la page d'amazon on teste les éléments de la page
         // "*[@id="twotabsearchtextbox"]" //pour copy xpath
@@ -24,9 +36,7 @@ public class tp1 {
 
         // autocomplétion "Ctrl+espace"
 
-        //Fermer le cookies
-        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
-        buttonCookies.click();
+
 
         // Rechercher dans la barre de recherche "machine a raclette"
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
@@ -42,12 +52,7 @@ public class tp1 {
     }
     @Test
     public void test2(){
-        WebDriver driver = new ChromeDriver(); // instantation pour ouvrir google
-        driver.get("https://www.amazon.fr"); // ouvrir la page amazon.fr
-        driver.manage().window().maximize();// maximiser la taille de l'écran
-        //Fermer le cookies
-        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
-        buttonCookies.click();
+
         // Rechercher dans la barre de recherche "machine a raclette"
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine a raclette");
