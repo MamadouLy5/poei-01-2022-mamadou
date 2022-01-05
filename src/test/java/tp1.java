a6b63a37c7c6fd1f402dadb9108927b49d34e5b3
Mame <mamely1847@gmail.com>
2022-01-05 15:10:30 +0100

    add explicitwait

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,7 +18,7 @@ public class tp1 {
     public void setup(){
         driver = new ChromeDriver(); // instantation pour ouvrir google
         // Temps d'attente de 2sec ici
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         driver.get("https://www.amazon.fr"); // ouvrir la page google.com
         driver.manage().window().maximize();// maximiser la taille de l'écran
@@ -49,7 +51,7 @@ public class tp1 {
         //driver.findElement(By.cssSelector(""));
         //driver.findElement(By.xpath("//*@id='twotabsearchtextbox'"));
         //driver.findElement(By.xpath("//*@name='field-keywords'"));
-        
+
     }
     @Test
     public void test2(){
@@ -76,5 +78,23 @@ public class tp1 {
         ajoutPanier.click();
           
 
+    }
+    @Test
+    public void testLivres(){
+        driver.findElement(By.id("nav-hamburger-menu")).click();
+        //Chronométre qui va attendre 10sec
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Explicit wait méthode qu'il faut utiliser pour cliquer sur un onglet aprés un temps d'attente
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector(".hmenu-item[data-menu-id='10']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")).click();
+        /*try {
+            Thread.sleep(3000); // Pour instancier un temps d'attente
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
     }
 }
